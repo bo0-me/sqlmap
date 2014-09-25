261f9062cd1a5e719863e97fb44223f15766b929
booka <boo@bo0.me>
2014-09-25 05:26:41 +0300

    log on exception

--- thirdparty/tic/tic.py
+++ thirdparty/tic/tic.py
@@ -7,17 +7,18 @@
 
 import urllib
 import xml.etree.ElementTree as ET
-
+import logging
 
 def get_tic(url):
     _ = 'http://bar-navig.yandex.ru/u?ver=2&show=1&url=%s'
 
     r = urllib.urlopen(_ % urllib.unquote(url).encode("utf8"))
+    rank = 0
     try:
         if r.code == 200:
             tree = ET.fromstring(r.read()).find('tcy')
             rank = int(tree.attrib['rang'])
-    except Exception:
-        rank = 0
+    except Exception as E:
+        logging.error("Exception during tic resolve by url: %s" % url)
 
     return rank
